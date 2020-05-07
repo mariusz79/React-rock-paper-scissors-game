207729206efd25bf56837adb14cc77c2a5996981
mariusz79 <mpawlowic@gmail.com>
2020-05-07 17:22:10 +0100

    Updates

---
+++ precache-manifest.77bce1a9af573ba43f9635faddc87865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b29c528f32823e8be7a35c24a83c4ef",
+    "url": "/ReactGame/index.html"
+  },
+  {
+    "revision": "a41cc6e69de07d9230ca",
+    "url": "/ReactGame/static/css/main.4e5801a3.chunk.css"
+  },
+  {
+    "revision": "6f01ac875bbd76cdeeba",
+    "url": "/ReactGame/static/js/2.abbfe05b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactGame/static/js/2.abbfe05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41cc6e69de07d9230ca",
+    "url": "/ReactGame/static/js/main.2f8e728a.chunk.js"
+  },
+  {
+    "revision": "52a87cde6d6be8a3a395",
+    "url": "/ReactGame/static/js/runtime-main.e333e01c.js"
+  }
+]);
